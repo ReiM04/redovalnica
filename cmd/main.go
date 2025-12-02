eeaf56812f205ae4d8d1a8997e2d8f2ff7328c9d
ReiM04 <66551685+ReiM04@users.noreply.github.com>
2025-12-02 20:27:14 +0100

    Add files via upload

---
+++ cmd/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+    "context"
+	"log"
+    "os"
+    "github.com/urfave/cli/v3"
+    "github.com/ReiM04/redovalnica/pkg"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name: "redovalnica",
+		Usage: "redovalnica za dodajanje in izpis ocen",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name: "stOcen",
+				Usage: "najmanjše število ocen potrebnih za pozitivno oceno",
+				Value: 6,
+			},
+			&cli.IntFlag{
+				Name: "minOcena",
+				Usage: "najmanjša možna ocena",
+				Value: 1,
+			},
+			&cli.IntFlag{
+				Name: "maxOcena",
+				Usage: "največja možna ocena",
+				Value: 10,
+			},
+		},
+
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			stOcen := cmd.Int("stOcen")
+			minOcena := cmd.Int("minOcena")
+			maxOcena := cmd.Int("maxOcena")
+
+			return run(stOcen, minOcena, maxOcena)
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(stOcen int, minOcena int, maxOcena int) error {
+
+	studenti := map[string]redovalnica.Student{
+		"63210001" : {Ime: "Ana", Priimek: "Novak", Ocene: []int{10, 9, 8}},
+		"63210002" : {Ime: "Boris", Priimek: "Kralj", Ocene:[]int{6,7,5,8}},
+		"63210003" : {Ime: "Janez", Priimek: "Novaj", Ocene:[]int{4,5,3,5}},
+	}
+
+	redovalnica.DodajOceno(studenti, "63210002", 10, minOcena, maxOcena)
+	redovalnica.DodajOceno(studenti, "63210002", 9, minOcena, maxOcena)
+
+	redovalnica.DodajOceno(studenti, "63210002", 42, minOcena, maxOcena)
+	redovalnica.DodajOceno(studenti, "12345678", 10, minOcena, maxOcena)
+
+	redovalnica.IzpisiKoncniUspeh(studenti, stOcen)
+
+	redovalnica.DodajOceno(studenti, "63210001", 9, minOcena, maxOcena)
+	redovalnica.DodajOceno(studenti, "63210001", 9, minOcena, maxOcena)
+	redovalnica.DodajOceno(studenti, "63210001", 9, minOcena, maxOcena)
+
+	redovalnica.IzpisVsehOcen(studenti)
+
+	redovalnica.DodajOceno(studenti, "63210003", 4, minOcena, maxOcena)
+	redovalnica.DodajOceno(studenti, "63210003", 4, minOcena, maxOcena)
+	redovalnica.DodajOceno(studenti, "63210003", 4, minOcena, maxOcena)
+
+	redovalnica.IzpisiKoncniUspeh(studenti, stOcen)
+
+	return nil
+}
---
+++ pkg/redovalnica.go
@@ -0,0 +1,61 @@
+package redovalnica
+
+import "fmt"
+
+
+type Student struct {
+	Ime 	string
+	Priimek string
+	Ocene   []int
+}
+
+func DodajOceno(studenti map[string]Student, vpisnaStevilka string, ocena int, minOcena int, maxOcena int) {
+	if ocena < minOcena || ocena > maxOcena {
+		fmt.Println("Ocena ni v med", minOcena , " in " maxOcena)
+		return
+	}
+
+	e, ok := studenti[vpisnaStevilka]
+	if ok {
+		e.ocene = append(e.ocene, ocena)
+		studenti[vpisnaStevilka] = e
+	} else {
+		fmt.Println("Studenta ni v seznamu")
+	}
+	return
+}
+
+func povprecje(studenti map[string]Student, vpisnaStevilka string, stOcen int) float64 {
+	e, ok := studenti[vpisnaStevilka]
+	if !ok {return -1}
+	if len(e.ocene) < stOcen {return 0.0}
+	var povprecje int
+	for _, v := range e.ocene {
+		povprecje = povprecje + v
+	}
+
+	return (float64(povprecje) / float64(len(e.ocene)))
+}
+
+func IzpisVsehOcen(studenti map[string]Student) {
+	fmt.Println("REDOVALNICA:")
+	for i, v := range studenti {
+		fmt.Printf("%s - %s %s: %v\n", i, v.Ime,v.Priimek, v.ocene)
+	}
+	return
+}
+
+func IzpisiKoncniUspeh(studenti map[string]Student, stOcen int) {
+	for i, v := range studenti {
+		var ocena = povprecje(studenti, i, stOcen)
+
+		if ocena >= 9 {
+			fmt.Printf("%s %s: povprecna ocena %.1f -> Odličen študent!\n", v.Ime, v.Priimek, ocena)
+		} else if ocena > 6 {
+			fmt.Printf("%s %s: povprecna ocena %.1f -> Povprečen študent!\n", v.Ime, v.Priimek, ocena)
+		} else {
+			fmt.Printf("%s %s: povprecna ocena %.1f -> Neuspešen študent!\n", v.Ime, v.Priimek, ocena)
+		}
+	}
+	return
+}
